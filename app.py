1a4906374383db6c419d7bf902e7696d4bd86d38
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-05-17 16:01:34 +1200

    App just echoes back

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 from bottle import route, run
 
-@route('/hello/:name')
-def index(name='World'):
-    return '<b>Hello %s!</b>' % name
+@route('/calculate/:variable_name')
+def index(variable_name='World'):
+    return 'calculation for {variable_name} goes here'.format(variable_name=variable_name)
 
 # Get required port, default to 5000.
 port = os.environ.get('PORT', 5000)
