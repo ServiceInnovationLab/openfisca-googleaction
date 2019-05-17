f8b93e80acbefaa686434c6e81969fe645bb9b16
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-05-17 16:11:55 +1200

    Starting to read request from google

--- app.py
+++ app.py
@@ -1,9 +1,70 @@
 import os
 from bottle import route, run
 
+
+
+response = {
+  "fulfillmentText": "This is a text response",
+  "fulfillmentMessages": [
+    {
+      "card": {
+        "title": "card title",
+        "subtitle": "card text",
+        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+        "buttons": [
+          {
+            "text": "button text",
+            "postback": "https://assistant.google.com/"
+          }
+        ]
+      }
+    }
+  ],
+  "source": "example.com",
+  "payload": {
+    "google": {
+      "expectUserResponse": true,
+      "richResponse": {
+        "items": [
+          {
+            "simpleResponse": {
+              "textToSpeech": "this is a simple response"
+            }
+          }
+        ]
+      }
+    },
+    "facebook": {
+      "text": "Hello, Facebook!"
+    },
+    "slack": {
+      "text": "This is a text response for Slack."
+    }
+  },
+  "outputContexts": [
+    {
+      "name": "projects/{PROJECT_ID}/agent/sessions/{SESSION_ID}/contexts/context name",
+      "lifespanCount": 5,
+      "parameters": {
+        "param": "param value"
+      }
+    }
+  ],
+  "followupEventInput": {
+    "name": "event name",
+    "languageCode": "en-US",
+    "parameters": {
+      "param": "param value"
+    }
+  }
+}
+
+
 @route('/calculate/:variable_name')
 def index(variable_name='World'):
-    return 'calculation for {variable_name} goes here'.format(variable_name=variable_name)
+    request = request.body.json()
+    print(request)
+    return {}
 
 # Get required port, default to 5000.
 port = os.environ.get('PORT', 5000)
