6d2b931057120e398d3d40f8ad51d628ed54e646
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-05-17 16:24:47 +1200

    Just say one million dollars

--- app.py
+++ app.py
@@ -2,63 +2,14 @@ import os
 from bottle import route, run, request
 
 
-
-def make_response(project_id, session_id):
-  return {
-    "fulfillmentText": "This is a text response",
-    "fulfillmentMessages": [
-      {
-        "card": {
-          "title": "card title",
-          "subtitle": "card text",
-          "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-          "buttons": [
-            {
-              "text": "button text",
-              "postback": "https://assistant.google.com/"
-            }
-          ]
-        }
-      }
-    ],
-    "source": "example.com",
-    "payload": {
-      "google": {
-        "expectUserResponse": True,
-        "richResponse": {
-          "items": [
-            {
-              "simpleResponse": {
-                "textToSpeech": "this is a simple response"
-              }
-            }
-          ]
-        }
-      }
-    },
-    "outputContexts": [
-      {
-        "name": "projects/{PROJECT_ID}/agent/sessions/{SESSION_ID}/contexts/context name".format(PROJECT_ID=project_id, SESSION_ID=session_id),
-        "lifespanCount": 5,
-        "parameters": {
-          "param": "param value"
-        }
-      }
-    ],
-    "followupEventInput": {
-      "name": "event name",
-      "languageCode": "en-US",
-      "parameters": {
-        "param": "param value"
-      }
-    }
-  }
-
-
 @route('/calculate/:variable_name')
 def index(variable_name='World'):
-    print(request.json)
-    return make_response("1", "1")
+    # json_request = request.json
+    # request_id = json_request['re']
+    return {
+      "fulfillmentText": "one million dollars",
+      "source": "https://ratesrebates.services.govt.nz"
+    }
 
 # Get required port, default to 5000.
 port = os.environ.get('PORT', 5000)
