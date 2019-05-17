395a3f2618445240cfe0f19e69d35565b71670d9
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-05-17 16:30:28 +1200

    Read parameters from the request

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import os
 from bottle import route, run, request
 
 
-@route('/calculate/:variable_name')
-def index(variable_name='World'):
-    # json_request = request.json
+@route('/calculate/rebate')
+def rebate():
+    json_request = request.json
+    dependents = json_request['parameters']['dependents']
+    income = json_request['parameters']['income']
+    rates = json_request['parameters']['rates']
     # request_id = json_request['re']
     return {
       "fulfillmentText": "one million dollars",
