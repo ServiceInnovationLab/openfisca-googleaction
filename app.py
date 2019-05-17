c0d8ecee3261c069554da667dfb97aec6db67c81
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-05-17 16:18:38 +1200

    Read the response and print it

--- app.py
+++ app.py
@@ -1,73 +1,67 @@
 import os
-from bottle import route, run
+from bottle import route, run, request
 
 
 
-response = {
-  "fulfillmentText": "This is a text response",
-  "fulfillmentMessages": [
-    {
-      "card": {
-        "title": "card title",
-        "subtitle": "card text",
-        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-        "buttons": [
-          {
-            "text": "button text",
-            "postback": "https://assistant.google.com/"
-          }
-        ]
-      }
-    }
-  ],
-  "source": "example.com",
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "this is a simple response"
+def make_response(project_id, session_id):
+  return {
+    "fulfillmentText": "This is a text response",
+    "fulfillmentMessages": [
+      {
+        "card": {
+          "title": "card title",
+          "subtitle": "card text",
+          "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+          "buttons": [
+            {
+              "text": "button text",
+              "postback": "https://assistant.google.com/"
             }
-          }
-        ]
+          ]
+        }
+      }
+    ],
+    "source": "example.com",
+    "payload": {
+      "google": {
+        "expectUserResponse": True,
+        "richResponse": {
+          "items": [
+            {
+              "simpleResponse": {
+                "textToSpeech": "this is a simple response"
+              }
+            }
+          ]
+        }
       }
     },
-    "facebook": {
-      "text": "Hello, Facebook!"
-    },
-    "slack": {
-      "text": "This is a text response for Slack."
-    }
-  },
-  "outputContexts": [
-    {
-      "name": "projects/{PROJECT_ID}/agent/sessions/{SESSION_ID}/contexts/context name",
-      "lifespanCount": 5,
+    "outputContexts": [
+      {
+        "name": "projects/{PROJECT_ID}/agent/sessions/{SESSION_ID}/contexts/context name".format(PROJECT_ID=project_id, SESSION_ID=session_id),
+        "lifespanCount": 5,
+        "parameters": {
+          "param": "param value"
+        }
+      }
+    ],
+    "followupEventInput": {
+      "name": "event name",
+      "languageCode": "en-US",
       "parameters": {
         "param": "param value"
       }
     }
-  ],
-  "followupEventInput": {
-    "name": "event name",
-    "languageCode": "en-US",
-    "parameters": {
-      "param": "param value"
-    }
   }
-}
 
 
 @route('/calculate/:variable_name')
 def index(variable_name='World'):
-    request = request.body.json()
-    print(request)
-    return {}
+    print(request.json)
+    return make_response("1", "1")
 
 # Get required port, default to 5000.
 port = os.environ.get('PORT', 5000)
 
 # Run the app.
-run(host='0.0.0.0', port=port)
+run(host='0.0.0.0', port=port, reloader=True)
